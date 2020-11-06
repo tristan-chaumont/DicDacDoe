cfeac330dbc9247c31a750b9f1e2451531ee6832
noirotquentin <quentin.noir@gmail.com>
2020-11-06 15:34:28 +0100

    Structure Morpion et 2D

--- src/Morpion/Morpion_2D.java
+++ src/Morpion/Morpion_2D.java
@@ -1,4 +1,55 @@
 package Morpion;
 
-public class Morpion_2D {
+public class Morpion_2D extends StructureMorpion{
+
+    public Morpion_2D() {
+        super(16);
+    }
+
+
+    @Override
+    public boolean solutionLine(int cell, char state) {
+        int begin = cell - cell%4;
+        for(int i = begin;i<begin+4;i++){
+            if (this.cells[i] != state){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean solutionColumn(int cell, char state) {
+        int begin = cell%4;
+        for(int i = begin;i<16;i=i+4){
+            if (this.cells[i] != state){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean solutionDiagonal(int cell, char state) {
+        if(cell%5 == 0){
+            for(int i =0;i<16;i=i+5){
+                if (this.cells[i] != state){
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        else if(cell%3 == 0){
+            for(int i =3;i<13;i=i+3){
+                if (this.cells[i] != state){
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        return false;
+
+    }
 }
--- src/Morpion/StructureMorpion.java
+++ src/Morpion/StructureMorpion.java
@@ -1,14 +1,27 @@
 package Morpion;
 
-public abstract class Structure_Morpion {
+import java.util.Arrays;
 
-    int [] cells;
+public abstract class StructureMorpion {
+
+    char [] cells;
 
 
-    Structure_Morpion(int size){
-        cells = new int[size];
+    StructureMorpion(int size){
+        cells = new char[size];
+        Arrays.fill(cells,' ');
     }
 
-    public abstract boolean find_solution_from_cell(int cell);
+    public boolean findSolutionFromCell(int cell){
+        char state = this.cells[cell];
+        return (solutionLine(cell,state) || solutionColumn(cell,state) || solutionDiagonal(cell,state));
+    };
+
+    public abstract boolean solutionLine(int cell,char state);
+
+    public abstract boolean solutionColumn(int cell,char state);
+
+    public abstract boolean solutionDiagonal(int cell,char state);
 
 }
+
