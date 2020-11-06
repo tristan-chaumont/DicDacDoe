5faf02d63fe27e93202b0d174ccf140274703af6
tristan-chaumont <tristan.chaumont68@gmail.com>
2020-11-06 14:44:09 +0100

    Read file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n-dimension (2 or 3): ");
+        int dimension;
+
+        // Ask user to write dimension he wants to use (2 or 3)
+        while(true) {
+            try {
+                dimension = Integer.parseInt(sc.nextLine());
+                if (dimension == 2 || dimension == 3)
+                    break;
+                else
+                    throw new NumberFormatException();
+                // not positive.
+            } catch (NumberFormatException e) {
+                // not an integer
+                System.out.print("n-dimension (2 or 3): ");
+            }
+        }
+
+        // Read file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("files/2D_Blank.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
