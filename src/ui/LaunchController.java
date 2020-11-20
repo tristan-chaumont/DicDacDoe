33cc74ae05f9b0067cd40f0b17ddb0c1c611f3e1
tristan-chaumont <tristan.chaumont68@gmail.com>
2020-11-20 14:01:30 +0100

    Dynamic information displaying on window

--- src/ui/LaunchController.java
+++ src/ui/LaunchController.java
@@ -2,35 +2,28 @@ package ui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.SVGPath;
-
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class LaunchController implements Initializable {
+
+    //region ATTRIBUTS FXML
 
     @FXML
     private AnchorPane parent;
 
     @FXML
     private HBox titleBar;
-
-    @FXML
-    private Label minimizeButton;
 
     @FXML
     private GridPane tictactoe2D;
@@ -42,17 +35,31 @@ public class LaunchController implements Initializable {
     private RadioButton radioButtonCircle;
 
     @FXML
-    private Label timer;
+    private TextArea information;
+
+    //endregion
+
+    //region AUTRES ATTRIBUTS
 
     private double xOffSet = 0;
     private double yOffSet = 0;
 
+    private final ToggleGroup radioButtonsGroup = new ToggleGroup();
+
+    private int currentPlayer;
+
+    //endregion
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         makeStageDraggable();
-        createTictactoe2D();
         linkRadioButtons();
+        createTictactoe2D();
+        information.setWrapText(true);
+        information.setText("Player can start");
     }
+
+    //region GESTION DE LA TITLE BAR CUSTOM
 
     private void makeStageDraggable() {
         titleBar.setOnMousePressed((event) -> {
@@ -75,6 +82,10 @@ public class LaunchController implements Initializable {
         System.exit(0);
     }
 
+    //endregion
+
+    //region GESTION DU TICTACTOE
+
     private void createTictactoe2D() {
         int rows = 4, columns = 4;
 
@@ -93,20 +104,26 @@ public class LaunchController implements Initializable {
                     stackPane.getStyleClass().add("first-cell");
                 }
 
+                final int finalI = i;
+                final int finalJ = j;
+
                 stackPane.setOnMouseClicked(event -> {
                     SVGPath icon = new SVGPath();
                     // CROSS
                     if (event.getButton() == MouseButton.PRIMARY) {
-                        icon.setContent("m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z");
-                        icon.setFill(Color.web("#e63946"));
-                        icon.setScaleX(0.08);
-                        icon.setScaleY(0.08);
-                    // CIRCLE
-                    } else if(event.getButton() == MouseButton.SECONDARY) {
-                        icon.setContent("m257.778 515.556c-142.137 0-257.778-115.642-257.778-257.778s115.641-257.778 257.778-257.778 257.778 115.641 257.778 257.778-115.642 257.778-257.778 257.778zm0-451.112c-106.61 0-193.333 86.723-193.333 193.333s86.723 193.333 193.333 193.333 193.333-86.723 193.333-193.333-86.723-193.333-193.333-193.333z");
-                        icon.setFill(Color.web("#ffe3a7"));
-                        icon.setScaleX(0.07);
-                        icon.setScaleY(0.07);
+                        if (currentPlayer == 0) {
+                            icon.setContent("m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z");
+                            icon.setFill(Color.web("#e63946"));
+                            icon.setScaleX(0.08);
+                            icon.setScaleY(0.08);
+                        } else {
+                            icon.setContent("m257.778 515.556c-142.137 0-257.778-115.642-257.778-257.778s115.641-257.778 257.778-257.778 257.778 115.641 257.778 257.778-115.642 257.778-257.778 257.778zm0-451.112c-106.61 0-193.333 86.723-193.333 193.333s86.723 193.333 193.333 193.333 193.333-86.723 193.333-193.333-86.723-193.333-193.333-193.333z");
+                            icon.setFill(Color.web("#ffe3a7"));
+                            icon.setScaleX(0.07);
+                            icon.setScaleY(0.07);
+                        }
+                        currentPlayer = Math.abs(currentPlayer - 1);
+                        information.setText(String.format("%s\n%s played column %d, line %d", information.getText(), currentPlayer == 1 ? "Cross" : "Circle", finalJ + 1, finalI + 1));
                     }
                     Group group = new Group(icon);
                     group.setTranslateX(2);
@@ -118,24 +135,35 @@ public class LaunchController implements Initializable {
         }
     }
 
+    //endregion
+
+    //region FONCTIONS QUI GERENT LA FENETRE DES PARAMETRES
+
     private void linkRadioButtons() {
-        final ToggleGroup group = new ToggleGroup();
-        radioButtonCross.setToggleGroup(group);
+        radioButtonCross.setToggleGroup(radioButtonsGroup);
         radioButtonCross.setSelected(true);
 
-        radioButtonCircle.setToggleGroup(group);
+        radioButtonCircle.setToggleGroup(radioButtonsGroup);
 
-        System.out.println(group.getSelectedToggle());
+        System.out.println(radioButtonsGroup.getSelectedToggle());
     }
 
-    private void resizeSvg(SVGPath svg, double width, double height) {
-        double originalWidth = svg.prefWidth(-1);
-        double originalHeight = svg.prefHeight(originalWidth);
-
-        double scaleX = width / originalWidth;
-        double scaleY = height / originalHeight;
-
-        svg.setScaleX(scaleX);
-        svg.setScaleY(scaleY);
+    /**
+     * Gère l'appuie sur le bouton "New game".
+     * Vérifie quelle forme a été choisie par le joueur.
+     */
+    @FXML
+    private void handleNewGameClick(MouseEvent event) {
+        currentPlayer = radioButtonsGroup.getSelectedToggle().equals(radioButtonCross) ? 0 : 1;
+        tictactoe2D.getChildren().clear();
+        createTictactoe2D();
     }
+
+    //endregion
+
+    //region FONCTIONS QUI GERENT LA FENETRE DES INFORMATIONS
+
+    //TODO
+
+    //endregion
 }
